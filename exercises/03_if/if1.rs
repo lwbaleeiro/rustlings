4628ca2d4cdb36eac27dcd76e0744ac5d1996fc7
Luan Baleeiro <dev.baleeiro@gmail.com>
2026-02-26 10:00:05 -0300

    feat: Complete Rustlings exercises `if1`, `if2`, and `if3`.

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,12 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b { a } else { b }
 }
 
 fn main() {
     // You can optionally experiment here.
+    println!("{}", bigger(32, 20));
 }
 
 // Don't mind this for now :)
