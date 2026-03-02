1bb65126b610e9c52ad04811652626ee6775b9ad
Luan Baleeiro <dev.baleeiro@gmail.com>
2026-03-01 23:41:16 -0300

    strings in Rust

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,20 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    format!("{} world!", input)
+
+    // Outras formas pode ser assim \/
+    //format!("{input} world!")
+    //input.to_owned() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
